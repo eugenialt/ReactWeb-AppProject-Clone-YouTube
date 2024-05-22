24369d37ec2c75d0ca86f3fbe6b5b2b86ef1e48d
Eugenia Litvintsava <26181412mail.ru@gmail.com>
2024-05-22 17:56:11 +0300

    Home page & Video static start

---
+++ my-yt/src/Pages/Video/Video.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PlayVideo from '../../Components/PlayVideo/PlayVideo'
+import './Video.css'
+import Recommended from '../../Components/Recommended/Recommended'
+const Video = () => {
+  return (
+    <div className='play-container'>
+      <PlayVideo />
+      <Recommended />
+    </div>
+  )
+}
+
+export default Video
